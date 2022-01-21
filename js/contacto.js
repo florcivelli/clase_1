39f779533ca927a29fa3a2406e504b2e139d0136
Florencia Civelli <florcivelli@hotmail.com>
2022-01-21 17:25:43 -0300

    agrego_jQuery

--- js/contacto.js
+++ js/contacto.js
@@ -25,3 +25,21 @@ $('#formcontacto').submit( function (event) {
     console.log(name, email, asunto, mensaje)
     
 })
+
+const URL_JSON = "../db/data.json"
+//Agregamos un botón con jQuery
+$("body").prepend('<button id="btn1">JSON</button>');
+//Escuchamos el evento click del botón agregado
+$("#btn1").click(() => { 
+$.getJSON(URL_JSON, function (respuesta, estado) {
+    if(estado === "success"){
+      let misDatos = respuesta;
+      for (const dato of misDatos) {
+        $("body").prepend(`<div>
+                                <h3>${dato.name}</h3>
+                                <p> ${dato.email}</p>
+                            </div>`)
+      }  
+    }
+    });
+});
